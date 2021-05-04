71eb082c51d8ba19bd4270b69792fc314e53b95d
Hannes <65465954+HannesTheDev@users.noreply.github.com>
2021-05-04 22:39:37 +0200

    Add files via upload

--- src/HannesTheDev/ItemID/Main.php
+++ src/HannesTheDev/ItemID/Main.php
@@ -1,1 +1,40 @@
+<?php
 
+namespace HannesTheDev\ItemID;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase
+{
+
+    public $config;
+    public $prefix;
+
+    public function onEnable()
+    {
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->prefix = $this->config->get("prefix");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool
+    {
+        switch ($cmd->getName()) {
+            case "id":
+                if ($sender instanceof Player) {
+                    $item = $sender->getInventory()->getItemInHand();
+                    $itemid = $this->config->get("itemid");
+                    $itemid = str_replace('{id}', $item->getId(), $itemid);
+                    $itemid = str_replace('{damage}', $item->getDamage(), $itemid);
+                    $itemid = str_replace('{block_name}', $item->getName(), $itemid);
+                    $sender->sendMessage($itemid);
+                }
+                break;
+        }
+        return true;
+    }
+}
